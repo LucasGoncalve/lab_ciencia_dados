b073f93bbfe18bf19a8885d7a7a551a857bd7c4f
LucasGoncalve <luolgoncalves@hotmail.com>
2021-04-14 21:14:53 -0300

    Primeiro commit

--- script_criacao.R
+++ script_criacao.R
@@ -1,3 +1,7 @@
 
 #criar a estrutura GIT para esse projeto
 usethis::use_git()
+usethis::use_github()
+usethis::create_github_token()
+gitcreds::gitcreds_set(URL = "http:\\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")
+
